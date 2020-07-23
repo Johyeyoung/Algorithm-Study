0e65a28c8208d57d646b8a33c59ed8d7615e49f3
Jung-Minji <52596617+Jung-Minji@users.noreply.github.com>
2020-07-23 10:18:37 +0900

    Update 9095.cpp

--- Dynamic_programming/9095.cpp
+++ Dynamic_programming/9095.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 using namespace std;
 
-// sol1) 재귀로 풀기 
-
-int result;
-void printSumCount(int n){
-
-	if(n == 0){
-		result++;
-		return;
-	}
-
-
-	if(n-1 >= 0) printSumCount(n-1);
-	if(n-2 >= 0) printSumCount(n-2);
-	if(n-3 >= 0) printSumCount(n-3);
-}
+// sol2) dp로 
+// dp : n-1 또는 n-2 또는 n-3으로 부분 문제 만들기
 
 int main() {
 	int T,n; cin >> T;
+	int dp[11];
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
 
 	for(int testCase = 0; testCase < T; testCase++){
 		cin >> n;
-		result = 0;
-		printSumCount(n);
-		cout << result << endl;
-
+		for(int i = 4; i <= n; i++){
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		cout << dp[n] << endl;
 	}
 
 	return 0;
