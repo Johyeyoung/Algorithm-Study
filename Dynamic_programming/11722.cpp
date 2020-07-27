cb24a461af3c4c1aea90bebc7904fc3903cf675b
Jung-Minji <52596617+Jung-Minji@users.noreply.github.com>
2020-07-27 16:42:02 +0900

    가장 긴 감소하는 부분 수열

--- Dynamic_programming/11722.cpp
+++ Dynamic_programming/11722.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define MAX 1000
+int main() {
+	int N, result = -1; cin >> N;
+	int arr[1001];
+	int dp[1001];
 
-int arr[MAX + 1]; // 수열
-int dp[MAX + 1];
-// dp[i] : 수열 맨 처음부터 i-1번째까지에서 가장 긴 감소하는 부분 수열의 길이
+	for(int i = 0; i < N; i++) cin >> arr[i];
 
-
-int main()
-{
- 
-	int n; 
-  cin >> n;
-	int result = 0; // 최대값 저장
-
-	for (int i = 1; i <= n; ++i){
-		cin >> arr[i];
-	}
-
-	for (int i = 1; i <= n; ++i) {
-		dp[i] = 1; 
-		for (int j = 0; j < i; ++j) {
-			if (arr[j] > arr[i] && dp[i] < dp[j] + 1){
-				dp[i] = dp[j] + 1;  	 
+	for(int i = 0; i < N; i++){
+		dp[i] = 1;
+		for(int j = 0; j < i; j++){
+			if(arr[j] > arr[i] && dp[j] + 1 > dp[i]){
+				dp[i] = dp[j] + 1;
 			}
 		}
-
-		if (result < dp[i]) result = dp[i];
-		
+		result = max(result, dp[i]);
 	}
 
-	cout << result << "\n";
+	// for(int i = 0; i < N; i++) cout << dp[i] << " ";
+	// cout << endl;
+	cout << result << endl;
+
+	return 0;
 }
- 
