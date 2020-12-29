a2ebed361a687f501ebe169d24be0e2189647c52
JungMin-ji <52596617+JungMin-ji@users.noreply.github.com>
2020-12-29 18:34:18 +0900

    인프런

--- 정민지/Binary_search/1. 이분검색.py
+++ 정민지/Binary_search/1. 이분검색.py
@@ -4,44 +4,20 @@ import sys
 N, M = map(int, input().split())
 lst = list(map(int, input().split()))
 
-lst.sort()
-print(lst.index(M) + 1)
 
 # 이분검색
-# lower = 0
-# upper = N-1
-#
-# while lower <= upper:
-#     mid = (lower + upper) // 2
-#     if lst[mid] == M:
-#         print(mid + 1)
-#         break
-#     elif lst[mid] < M:
-#         lower = mid + 1
-#     else:
-#         upper = mid - 1
+lower = 0
+upper = N-1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+while lower <= upper:
+    mid = (lower + upper) // 2
+    if lst[mid] == M:
+        print(mid + 1)
+        break
+    elif lst[mid] < M:
+        lower = mid + 1
+    else:
+        upper = mid - 1
+        
+# lst.sort()
+# print(lst.index(M) + 1)
