648c386a986fdbcb722a810d7e45b76fc25701ad
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2021-01-14 00:50:18 +0900

    Update [Programmers]Lv 3.정수삼각형.py

--- 조혜영/DP/[Programmers]Lv 3.정수삼각형.py
+++ 조혜영/DP/[Programmers]Lv 3.정수삼각형.py
@@ -5,12 +5,12 @@
 # ----------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------
 
-def count(line, idx):
+def count(line, idx): # 이 함수의 목적은 return dy[line][idx]임 
     if dy[line][idx] > 0:
-        return dy[line][idx]
+        return dy[line][idx] # 이 함수의 목적은 return dy[line][idx]임 
     
     if line == 0 and idx == 0:
-        return dy[line][idx]
+        return tri[0][0] # 이 함수의 목적은 return dy[line][idx]임 
     else:
         if idx == 0:
             dy[line][idx] = count(line - 1, idx) + tri[line][idx]
@@ -19,7 +19,7 @@ def count(line, idx):
         else:
             dy[line][idx] = max(count(line-1, idx), count(line-1, idx-1)) + tri[line][idx]
 
-        return dy[line][idx]
+        return dy[line][idx] # 이 함수의 목적은 return dy[line][idx]임 
 
 
 def solution(triangle):
@@ -27,7 +27,6 @@ def solution(triangle):
     global tri, dy
     tri = triangle
     dy = [[0]* len(triangle) for _ in range(len(triangle))]
-    dy[0][0] = triangle[0][0]
     max = 0
     for i in range(len(triangle)):
         x = count(len(triangle)-1, i)
