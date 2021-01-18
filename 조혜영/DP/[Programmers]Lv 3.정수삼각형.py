8a2ec232586a7c70e5ba101a3a47ec6c84a1bd38
dodo486 <47061005+Johyeyoung@users.noreply.github.com>
2021-01-18 12:35:52 +0900

    2차원 map에서의 DP

--- 조혜영/DP/[Programmers]Lv 3.정수삼각형.py
+++ 조혜영/DP/[Programmers]Lv 3.정수삼각형.py
@@ -12,6 +12,7 @@ def count(line, idx): # 이 함수의 목적은 return dy[line][idx]임
     if line == 0 and idx == 0:
         return tri[0][0] # 이 함수의 목적은 return dy[line][idx]임 
     else:
+        # 가지고 있는 방향 중에 가장 큰 값을 선택하는 점화식도출 (예외: 한 방향만 있는 경우는  )
         if idx == 0:
             dy[line][idx] = count(line - 1, idx) + tri[line][idx]
         elif idx == len(tri[line]) - 1:
