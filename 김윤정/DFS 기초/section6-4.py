277aef17c6099ce3237004295a19701f97e2f9b0
Yunjoung808 <s_holmes25@naver.com>
2022-02-19 23:26:01 +0900

    section 6-5

--- 김윤정/DFS 기초/section6-4.py
+++ 김윤정/DFS 기초/section6-4.py
@@ -1,1 +1,22 @@
 #합이 같은 부분집합
+#L은 a list의 인덱스 번호
+
+import sys
+
+def DFS(L, sum):
+    if L==n:
+        if sum == (total-sum):
+            print("YES")
+            sys.exit(0)
+        else:
+            DFS(L+1, sum+a[L])
+            DFS(L+1, sum)
+
+
+
+if __name__ =="__main__":
+    n=int(input())
+    a=list(map(int, input().split()))
+    total = sum(a)
+    DFS(0,0)
+    print("NO")
---
+++ 김윤정/DFS 기초/section6-5.py
@@ -0,0 +1,19 @@
+#바둑이 승차
+def DFS(L, sum):
+    if sum > c:
+        return
+    if L == n:
+        if sum>result:
+            result=sum
+    else:
+        DFS(L+1, sum+a[L])
+        DFS(L+1, sum)
+
+if __name__ == '__main__':
+    c, n = map(int, input().split())
+    a = [0]*n
+    result = - 2147000000
+    for i in range(n):
+        a[i]=int(input())
+    DFS(0,0)
+    print(result)
